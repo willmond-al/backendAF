4366feef3cefa79c0d7f93061c9fdf64eb70ab41
willmondal <wbmondal@aol.com>
2021-03-02 17:40:10 -0800

    change to seeds

--- api/data/seeds/02-clients.js
+++ api/data/seeds/02-clients.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('clients').insert([
-        {id: 1, username: 'Ronnie Coleman', password: "password"},
-        {id: 2, username: 'Arnold Schawrzenegger', password: "password"},
-        {id: 3, username: 'Jay Cutler', password: "password"}
+        {id: 1, username: 'Ronnie Coleman', password: "$2a$10$ismIWy4E07ShjnVpnqshRunE7LlB2UInJpSOanyoJYZqMzHWvQP7i"},
+        {id: 2, username: 'Arnold Schawrzenegger', password: "$2a$10$QbTJf5wCaM9KjUkqD6UvjulziLShYU/4jbtsAU0s6EFqH/lR6.Vb."},
+        {id: 3, username: 'Jay Cutler', password: "$2a$10$7Z6t/mczmlABGgKkixabUOL..D5CtM1/9gziKNQ7g5O1vsdSHCDH."}
       ]);
     });
 };
